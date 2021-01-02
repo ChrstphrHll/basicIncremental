c00a52563524f6a646372a7a113a8fbc95ffc501
Christopher Hall II <christopher.s.hall@duke.edu>
2021-01-02 14:46:40 -0500

    Initial Commit

---
+++ basicIncrimental/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basicIncrimental
+//
+//  Created by Christopher Hall on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basicIncrimental/basicIncrimentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicIncrimentalApp.swift
+//  basicIncrimental
+//
+//  Created by Christopher Hall on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct basicIncrimentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
