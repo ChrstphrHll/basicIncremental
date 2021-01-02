7178957e37c5a5edc0a81585c7379b7bb9b84805
Christopher Hall II <christopher.s.hall@duke.edu>
2021-01-02 15:16:57 -0500

    loading saving implemented

--- basicIncrimental/ContentView.swift
+++ basicIncrimental/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+struct Inventory: Codable{
+    var red: Int
+    var blu: Int
+}
+
+class profileInfo: ObservableObject{
+    @Published var currency: Inventory{
+        didSet{
+            let encoder = JSONEncoder()
+            if let saved = try? encoder.encode(currency){
+                UserDefaults.standard.set(saved, forKey: "saved")
+            }
+        }
+    }
+    
+    init(){
+        if let saveData = UserDefaults.standard.data(forKey: "saved"){
+            let decoder = JSONDecoder()
+            if let loaded = try? decoder.decode(Inventory.self, from: saveData){
+                self.currency = loaded
+                return
+            }
+        }
+        
+        self.currency = Inventory(red: 0, blu: 0)
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var inven = profileInfo()
+    
     var body: some View {
-        Text("Git???")
-            .padding()
+        VStack {
+            Text("Red: \(inven.currency.red) Blu: \(inven.currency.blu)")
+                .padding()
+            
+            HStack{
+                Button(action: {
+                    self.inven.currency.red += 1
+                }, label: {
+                    Text("Red")
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                })
+                
+                Button(action: {
+                    self.inven.currency.blu += 1
+                }, label: {
+                    Text("Blu")
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+            }
+            
+            
+        }
     }
 }
 
