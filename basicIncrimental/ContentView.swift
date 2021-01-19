fa109f4dc1c3f80b8bbc7ea88ff6cf70d75daab8
Christopher Hall II <christopher.s.hall@duke.edu>
2021-01-18 22:09:34 -0500

    added hat buying!!!!!

--- basicIncrimental/ContentView.swift
+++ basicIncrimental/ContentView.swift
@@ -12,11 +12,23 @@ struct currencyType: Codable, Identifiable{
     var amount: Int
     var unlocked: Bool
     var id = UUID()
+    var hats: Int
     
     init(_ quick: String){
         name = quick
         amount = 0
-        unlocked = false
+        
+        if quick == "red"{
+            unlocked = true
+        } else {
+            unlocked = false
+        }
+        
+        hats = 0
+    }
+    
+    mutating func addHat(){
+        self.hats += 1
     }
     
     func getColor()->Color{
@@ -56,6 +68,32 @@ class profileInfo: ObservableObject{
         }
     }
     
+    //v similar to above getColor func; possibly integrate into one struct
+    func useCurr(color: String, cost: Int)->Bool{
+        var type = 0
+        switch color{
+        case "red":
+            type = 0
+        case "blue":
+            type = 1
+        case "green":
+            type = 2
+        case "purple":
+            type = 3
+        case "black":
+            type = 4
+        default:
+            type = -1
+        }
+        
+        if self.currency[type].amount >= cost{
+            self.currency[type].amount -= cost
+            return true
+        } else {
+            return false
+        }
+    }
+    
     init(debug: Bool = false){
         
         if let saveData = UserDefaults.standard.data(forKey: "saved"){
@@ -85,16 +123,26 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Spacer()
+            HStack {
+                Spacer()
+                
+                //Text("Red: \(profile.currency[0].amount) Blu: \(profile.currency[1].amount)")
+                //    .padding()
+                
+                tapperPanel(prof: profile)
+                
+                Spacer()
+                
+                store(profile: profile)
+            }
             
-            Text("Red: \(profile.currency[0].amount) Blu: \(profile.currency[1].amount)")
-                .padding()
-            
-            tapperPanel(prof: profile)
-            
-            Spacer()
-            
-            store(profile: profile)
+            HStack{
+                Text("red hats: \(profile.currency[0].hats)")
+                Text("blu hats: \(profile.currency[1].hats)")
+                Text("grn hats: \(profile.currency[2].hats)")
+                Text("prl hats: \(profile.currency[3].hats)")
+                Text("blk hats: \(profile.currency[4].hats)")
+            }
         }
     }
 }
--- basicIncrimental/store.swift
+++ basicIncrimental/store.swift
@@ -11,9 +11,84 @@ struct store: View {
     @ObservedObject var profile: profileInfo
     
     var body: some View {
-        Button("Red Hat"){
-            if self.profile.currency[0].amount >= 50{
-                self.profile.currency[0].amount -= 50
+        HStack {
+            VStack {
+                //compress
+                Button(action: {
+                    if profile.useCurr(color: "red", cost: 50){
+                        profile.currency[1].unlock()
+                    }
+                }){
+                    tapper(name: "Buy Blue", color: .red)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "blue", cost: 50){
+                        profile.currency[2].unlock()
+                    }
+                }){
+                    tapper(name: "Buy Green", color: .blue)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "green", cost: 50){
+                        profile.currency[3].unlock()
+                    }
+                }){
+                    tapper(name: "Buy Purple", color: .green)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "purple", cost: 50){
+                        profile.currency[4].unlock()
+                    }
+                }){
+                    tapper(name: "Buy Black", color: .purple)
+                }
+            }
+            
+            
+            VStack {
+                //compress
+                Button(action: {
+                    if profile.useCurr(color: "red", cost: 50){
+                        profile.currency[0].addHat()
+                    }
+                }){
+                    tapper(name: "Buy Red Hat", color: .red)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "blue", cost: 50){
+                        profile.currency[1].addHat()
+                    }
+                }){
+                    tapper(name: "Buy Blue Hat", color: .blue)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "green", cost: 50){
+                        profile.currency[2].addHat()
+                    }
+                }){
+                    tapper(name: "Buy Green Hat", color: .green)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "purple", cost: 50){
+                        profile.currency[3].addHat()
+                    }
+                }){
+                    tapper(name: "Buy Purple Hat", color: .purple)
+                }
+                
+                Button(action: {
+                    if profile.useCurr(color: "black", cost: 50){
+                        profile.currency[4].addHat()
+                    }
+                }){
+                    tapper(name: "Buy Black Hat", color: .black)
+                }
             }
         }
     }
